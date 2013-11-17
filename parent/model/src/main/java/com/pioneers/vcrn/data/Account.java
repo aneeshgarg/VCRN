cee1befe766a71b8f7ad150f728d2b5ce14b00e9
Aneesh Garg <aneeshgarg@gmail.com>
2013-11-16 21:08:34 -0500

    Initialiing Maven project

---
+++ parent/model/src/main/java/com/pioneers/vcrn/data/Account.java
@@ -0,0 +1,51 @@
+package com.pioneers.vcrn.data;
+
+public class Account {
+
+	private long accountId;
+	private String username;
+	private String password;
+	private boolean isActive;
+	private String role;
+
+	public long getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(long accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
